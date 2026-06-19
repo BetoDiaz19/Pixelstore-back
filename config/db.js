3aa8dc65de70e10632d4ef35bcec3d318f6d6b86
Luis Alberto Pedroza Diaz <132956794+BetoDiaz19@users.noreply.github.com>
2026-06-19 16:39:24 -0600

    Enhance DB connection with SSL and port settings
    Added SSL configuration and port setting for database connection.

--- config/db.js
+++ config/db.js
@@ -3,9 +3,13 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,          // ¡Nuevo! Le dice a qué puerto tocar
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {                              // ¡Nuevo! Obligatorio para entrar a Aiven
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((error) => {
